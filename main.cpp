1fea8a839cffa4b96fc4f081b032e77e8065ce59
tomstream <thomas@thomasdeMacBook-Pro.local>
2015-05-17 22:34:50 +0800

    some small improvement

--- main.cpp
+++ main.cpp
@@ -8,18 +8,17 @@ using namespace std::placeholders;
 int main()
 {
 
-	string s("sin(x)-x^2/4");
-
+	string s("sin(6+10*(5/2))-5^2/4");
 	string s1= infixToPostfix(s);
-	cout << s1 << endl;
 	cout << postfixToAnswer(s1, 2) << endl;;
 	
-	auto g = bind(postfixToAnswer, s1, _1);
+	//	//cout << s1 << endl;
+	//auto g = bind(postfixToAnswer, s1, _1);
 
-	Dichotomy D1(1.5, 2, 1e-5, g);
-	Dichotomy D2(1.5, 2, 1e-5, [](double x){return sin(x) - x*x / 4; });
+	//Dichotomy D1(1.5, 2, 1e-5, g);
+	//Dichotomy D2(1.5, 2, 1e-5, [](double x){return sin(x) - x*x / 4; });
 
-	cout << D1<<D2;
+	//cout << D1<<D2;
 	getchar();
 	return 0;
 }
